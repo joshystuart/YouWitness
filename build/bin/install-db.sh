7708673d7afa2a9f2c70004e346ae0d7df013295
Josh Stuart <josh.stuart@zoopcommerce.com>
2013-07-30 14:21:20 +1000

    Update DB

--- build/bin/install-db.sh
+++ build/bin/install-db.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+sh ./vendor/bin/doctrine-module orm:schema-tool:drop --force
+
 sh ./vendor/bin/doctrine-module orm:schema-tool:create
+
+sh ./vendor/bin/doctrine-module orm:generate-proxies
